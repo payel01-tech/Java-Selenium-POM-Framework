ccfe13126e33f12feef6056bc7d0bb1877ec3314
payel01-tech <payelbhattacharyaofficial@gmail.com>
2025-09-06 00:21:10 +0530

    Updated Utility Folder

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -3,11 +3,12 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utilities.DriverFactory;
+import utilities.WaitUtils;
 
 public class LoginPage {
 
     private WebDriver driver;
-
+    private WaitUtils waitUtils;
     private By usernameTextBox = By.id("Email");
     private By passwordTextBox = By.id("Password");
     private By checkBoxRememberMe =  By.xpath("//input[@type=\"checkbox\" and @id=\"RememberMe\"]");
@@ -16,27 +17,34 @@ public class LoginPage {
     public LoginPage()
     {
         this.driver= DriverFactory.getDriver();
+        waitUtils = new WaitUtils(driver,10);
     }
 
     public void enterUserName(String userName)
     {
+        waitUtils.waitForVisibility(driver.findElement(usernameTextBox));
         driver.findElement(usernameTextBox).clear();
         driver.findElement(usernameTextBox).sendKeys(userName);
     }
 
     public void enterPassword(String passWord)
     {
+        waitUtils.waitForVisibility(driver.findElement(passwordTextBox));
         driver.findElement(passwordTextBox).clear();
         driver.findElement(passwordTextBox).sendKeys(passWord);
     }
 
     public void setCheckBoxRememberMe()
     {
+        waitUtils.waitForClickable(driver.findElement(checkBoxRememberMe));
         driver.findElement(checkBoxRememberMe).click();
     }
 
     public void clickLogin()
     {
+        waitUtils.waitForClickable(driver.findElement(loginButton));
         driver.findElement(loginButton).click();
     }
 }
+
+
--- src/main/java/utilities/WaitUtils.java
+++ src/main/java/utilities/WaitUtils.java
@@ -1,4 +1,33 @@
 package utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class WaitUtils {
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    public WaitUtils(WebDriver driver, int timeout)
+    {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+    }
+
+    public WebElement waitForVisibility(WebElement element){
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public WebElement waitForClickable(WebElement element){
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public boolean waitForText(WebElement element, String text){
+        return wait.until(ExpectedConditions.textToBePresentInElement(element, text));
+    }
+
 }
